375b22e63ad97a70efe195c4266589951d6c32ac
zentim <zentim3248@gmail.com>
2019-09-06 14:52:14 +0800

    Add projects controller

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
+
+Route::get('/projects', 'ProjectsController@index');
