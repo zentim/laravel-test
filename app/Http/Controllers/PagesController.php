29f38234dbd588f9aa2a6f1980b1e9d6a692b91e
zentim <zentim3248@gmail.com>
2019-09-06 11:29:14 +0800

    Add controller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function home()
+    {
+        return view('welcome', [
+            'foo' => 'bar',
+            'tasks' => [
+                'hi',
+                'test'
+            ]
+        ]);
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
